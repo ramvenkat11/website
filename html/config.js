3a2acf51443c8de7fd50b697862c334641915d69
ramv <ram.venkat.11@gmail.com>
2026-09-21 21:14:50 -0400

    Move from s2oserver

--- html/config.js
+++ html/config.js
@@ -1,4 +1,4 @@
 window.SEARCH2O_CONFIG = {
   apiUrl: "https://reg.api.search2o.com",
-  demoUrl: "https://s2odemo-xu3srzx3wa-uk.a.run.app", // https://demo.api.search2o.com",
+  demoUrl: "https://demo.api.search2o.com",
 };
